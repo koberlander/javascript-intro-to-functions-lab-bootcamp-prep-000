741e3b98727063ffc5c2687bd4be6054c64a4642
ko kristin <koberlander115@gmail.com>
2019-01-13 01:18:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   
   if (string === mixcase){
     return "I love you, too.";
-  } else if (string === string.toUppercase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
    } else {
      return "I can't hear you!";
